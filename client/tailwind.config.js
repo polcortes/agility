aff386241e8985887db0391e36c874a5f963bb6d
polcortes <pcortesgarcia.cf@iesesteveterradas.cat>
2024-03-03 20:06:36 +0100

    added new fonts

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  darkMode: 'class',
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    fontFamily: {
+      'title': ['"Karla"'],
+      'subtitle': ['"Source Sans 3"'],
+      'body': ['"Montserrat"'],
+    }
   },
   plugins: [],
 }
