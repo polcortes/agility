45a97d8a629e6b2b6ec4857e1231bb0f442c6f90
polcortes <pcortesgarcia.cf@iesesteveterradas.cat>
2024-03-11 20:04:24 +0100

    Custom colors config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,7 +3,16 @@ export default {
   darkMode: 'class',
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-primary-bg': '#0F0C11',
+        'dark-secondary-bg': '#201822',
+        'dark-tertiary-bg': '#47334D',
+        'light-primary-bg': '#D7D7D7',
+        'light-secondary-bg': '#F5F5F5',
+        'the-accent-color': '#4F46E5'
+      },
+    },
     fontFamily: {
       'title': ['"Barlow"'],
       'subtitle': ['"Source Sans 3"'],
