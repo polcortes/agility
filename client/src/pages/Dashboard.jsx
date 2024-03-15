dea4e61748b4b477fc1630697c66623df3869233
polcortes <pcortesgarcia.cf@iesesteveterradas.cat>
2024-03-15 17:05:45 +0100

    Dashboard page changes

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,194 +1,52 @@
 // import { useTranslation } from 'react-i18next'
 import { Helmet } from 'react-helmet-async'
 
+import axios from 'axios'
+
 // import DarkIcon from '../assets/icons/dark-agility'
 import LightIcon from '../assets/icons/light-agility'
 
+import ProjectCard from '../components/ProjectCard'
+import CreateProject from '../components/CreateProject'
+import SearchProjects from '../components/SearchProjects'
+
+import { useEffect, useRef, useState } from 'react'
 
 const Dashboard = () => {
-  const projects = [
-    {
-      title: 'Projecte 1',
-      author: 'Erik Tamaño',
-      href: '/projecte-1'
-    },
-    {
-      title: 'Projecte 2',
-      author: 'Pol Cortés',
-      href: '/projecte-2'
-    },
-    {
-      title: 'Projecte 3',
-      author: 'Erik Tamaño',
-      href: '/projecte-3'
-    },
-    {
-      title: 'Projecte 4',
-      author: 'Erik Tamaño',
-      href: '/projecte-4'
-    },
-    {
-      title: 'Projecte 5',
-      author: 'Pol Cortés',
-      href: '/projecte-5'
-    },
-    {
-      title: 'Projecte 6',
-      author: 'Erik Tamaño',
-      href: '/projecte-6'
-    },
-    {
-      title: 'Projecte 1',
-      author: 'Erik Tamaño',
-      href: '/projecte-1'
-    },
-    {
-      title: 'Projecte 2',
-      author: 'Pol Cortés',
-      href: '/projecte-2'
-    },
-    {
-      title: 'Projecte 3',
-      author: 'Erik Tamaño',
-      href: '/projecte-3'
-    },
-    {
-      title: 'Projecte 4',
-      author: 'Erik Tamaño',
-      href: '/projecte-4'
-    },
-    {
-      title: 'Projecte 5',
-      author: 'Pol Cortés',
-      href: '/projecte-5'
-    },
-    {
-      title: 'Projecte 6',
-      author: 'Erik Tamaño',
-      href: '/projecte-6'
-    },
-    {
-      title: 'Projecte 1',
-      author: 'Erik Tamaño',
-      href: '/projecte-1'
-    },
-    {
-      title: 'Projecte 2',
-      author: 'Pol Cortés',
-      href: '/projecte-2'
-    },
-    {
-      title: 'Projecte 3',
-      author: 'Erik Tamaño',
-      href: '/projecte-3'
-    },
-    {
-      title: 'Projecte 4',
-      author: 'Erik Tamaño',
-      href: '/projecte-4'
-    },
-    {
-      title: 'Projecte 5',
-      author: 'Pol Cortés',
-      href: '/projecte-5'
-    },
-    {
-      title: 'Projecte 6',
-      author: 'Erik Tamaño',
-      href: '/projecte-6'
-    },
-    {
-      title: 'Projecte 1',
-      author: 'Erik Tamaño',
-      href: '/projecte-1'
-    },
-    {
-      title: 'Projecte 2',
-      author: 'Pol Cortés',
-      href: '/projecte-2'
-    },
-    {
-      title: 'Projecte 3',
-      author: 'Erik Tamaño',
-      href: '/projecte-3'
-    },
-    {
-      title: 'Projecte 4',
-      author: 'Erik Tamaño',
-      href: '/projecte-4'
-    },
-    {
-      title: 'Projecte 5',
-      author: 'Pol Cortés',
-      href: '/projecte-5'
-    },
-    {
-      title: 'Projecte 6',
-      author: 'Erik Tamaño',
-      href: '/projecte-6'
-    },
-    {
-      title: 'Projecte 1',
-      author: 'Erik Tamaño',
-      href: '/projecte-1'
-    },
-    {
-      title: 'Projecte 2',
-      author: 'Pol Cortés',
-      href: '/projecte-2'
-    },
-    {
-      title: 'Projecte 3',
-      author: 'Erik Tamaño',
-      href: '/projecte-3'
-    },
-    {
-      title: 'Projecte 4',
-      author: 'Erik Tamaño',
-      href: '/projecte-4'
-    },
-    {
-      title: 'Projecte 5',
-      author: 'Pol Cortés',
-      href: '/projecte-5'
-    },
-    {
-      title: 'Projecte 6',
-      author: 'Erik Tamaño',
-      href: '/projecte-6'
-    },
-    {
-      title: 'Projecte 1',
-      author: 'Erik Tamaño',
-      href: '/projecte-1'
-    },
-    {
-      title: 'Projecte 2',
-      author: 'Pol Cortés',
-      href: '/projecte-2'
-    },
-    {
-      title: 'Projecte 3',
-      author: 'Erik Tamaño',
-      href: '/projecte-3'
-    },
-    {
-      title: 'Projecte 4',
-      author: 'Erik Tamaño',
-      href: '/projecte-4'
-    },
-    {
-      title: 'Projecte 5',
-      author: 'Pol Cortés',
-      href: '/projecte-5'
-    },
-    {
-      title: 'Projecte 6',
-      author: 'Erik Tamaño',
-      href: '/projecte-6'
-    },
-    
-  ]
+  const createProjectRef = useRef(null)
+  
+  const [isCreateProjectShown, setIsCreateProjectShown] = useState(false);
+
+  const handleCloseCreateProject = () => {
+    setIsCreateProjectShown(false);
+  };
+
+  const [ isSearchProjectsShown, setIsSearchProjectsShown ] = useState(false);
+
+  const handleCloseSearchProjects = () => {
+    setIsSearchProjectsShown(false);
+  }
+
+  const [ projects, setProjects ] = useState([])
+
+  useEffect(() => {
+    axios
+      .post('http://localhost:3000/getProjects')
+      .then(res => {
+        setProjects(res.result)
+      })
+  })
+
+  const [ searchValue, setSearchValue ] = useState('')
+
+  const [ results, setResults ] = useState([])
+
+  useEffect(() => {
+    const newResults = projects.filter(project => project.title.toLowerCase().includes(searchValue.toLowerCase()))
+
+    if (newResults.length === 0) setResults(["No s'han trobat resultats"])
+    else setResults(newResults)
+  }, [searchValue, projects])
 
   return (
     <>
@@ -199,24 +57,39 @@ const Dashboard = () => {
 
       <main 
         className="
-          grid grid-rows-[82px_1fr] overflow-hidden gap-4
+          grid grid-rows-[82px_1fr] overflow-hidden gap-4 relative
           w-screen h-screen xl:p-4 box-border font-body transition-[padding]
           bg-gradient-to-t from-light-primary-bg/80 to-light-primary-bg"
       >
         <header 
           className='
-            p-5 rounded-md 
+            p-5 rounded-md relative 
             flex items-center justify-between
             bg-light-secondary-bg'
         >
           <LightIcon className={`w-64 h-auto`} />
 
-          <button>
+          <button
+            id="create-project-button"
+            className='bg-the-accent-color flex items-center justify-center rounded-md px-3 py-2 text-white font-medium hover:scale-105 transition-all gap-2 absolute right-1/2 translate-x-1/2'
+            onClick={() => setIsCreateProjectShown(!isCreateProjectShown)}
+          >
+            <svg width="20" height="20" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.49991 0.876892C3.84222 0.876892 0.877075 3.84204 0.877075 7.49972C0.877075 11.1574 3.84222 14.1226 7.49991 14.1226C11.1576 14.1226 14.1227 11.1574 14.1227 7.49972C14.1227 3.84204 11.1576 0.876892 7.49991 0.876892ZM1.82707 7.49972C1.82707 4.36671 4.36689 1.82689 7.49991 1.82689C10.6329 1.82689 13.1727 4.36671 13.1727 7.49972C13.1727 10.6327 10.6329 13.1726 7.49991 13.1726C4.36689 13.1726 1.82707 10.6327 1.82707 7.49972ZM7.50003 4C7.77617 4 8.00003 4.22386 8.00003 4.5V7H10.5C10.7762 7 11 7.22386 11 7.5C11 7.77614 10.7762 8 10.5 8H8.00003V10.5C8.00003 10.7761 7.77617 11 7.50003 11C7.22389 11 7.00003 10.7761 7.00003 10.5V8H4.50003C4.22389 8 4.00003 7.77614 4.00003 7.5C4.00003 7.22386 4.22389 7 4.50003 7H7.00003V4.5C7.00003 4.22386 7.22389 4 7.50003 4Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
             Crear projecte
           </button>
 
-          <span className="flex">
-            <input type="search" name="search-in-projects" id="search-in-projects" placeholder='Search' />
+          <span className="flex h-full relative">
+            <svg className='absolute top-1/2 -translate-y-1/2 left-4' width="20" height="20" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10 6.5C10 8.433 8.433 10 6.5 10C4.567 10 3 8.433 3 6.5C3 4.567 4.567 3 6.5 3C8.433 3 10 4.567 10 6.5ZM9.30884 10.0159C8.53901 10.6318 7.56251 11 6.5 11C4.01472 11 2 8.98528 2 6.5C2 4.01472 4.01472 2 6.5 2C8.98528 2 11 4.01472 11 6.5C11 7.56251 10.6318 8.53901 10.0159 9.30884L12.8536 12.1464C13.0488 12.3417 13.0488 12.6583 12.8536 12.8536C12.6583 13.0488 12.3417 13.0488 12.1464 12.8536L9.30884 10.0159Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
+            <input 
+              onFocus={() => setIsSearchProjectsShown(true)}
+              onChange={(e) => setSearchValue(e.target.value)}
+              className="bg-light-tertiary-bg rounded-md h-fit px-3 py-2 pl-12 outline-none flex items-center" 
+              type="search" 
+              name="search-in-projects" 
+              id="search-in-projects" 
+              placeholder='Search' 
+            />
+            <div className='w-[1px] h-full border-l-2 border-black mx-6'></div>
             <span className="bg-[#d7d7d7] size-12 flex items-center justify-center rounded-full overflow-hidden">
               pfp<br />icon
             </span>
@@ -228,21 +101,26 @@ const Dashboard = () => {
             flex flex-col items-center
             bg-light-secondary-bg"
         >
-          <h1 className="font-title font-extrabold text-[] text-5xl mb-6">Projectes</h1>
-          <section className='grid lg:grid-cols-3 gap-8 w-full md:px-24 px-6 sm:grid-cols-2 grid-cols-1'>
-            {projects.map((project, index) => {
+          <h1 className="font-title font-extrabold text-[] text-5xl mb-8">Projectes</h1>
+
+          <section className='grid lg:grid-cols-3 w-full gap-8 md:px-24 px-6 sm:grid-cols-2 grid-cols-1'>
+            {projects && typeof projects === Array && projects.map((project, index) => {
               return (
-                <article key={index} className="rounded-md shadow-lg">
-                  <a href={project.href} className='h-full w-full bg-indigo-50 hover:bg-indigo-200/80 grid grid-cols-[1fr_3fr] grid-rows-2 p-3 rounded-md'>
-                    <span className="size-16 mr-5 bg-black row-span-2 rounded-md"></span>
-                    <h2 className="font-medium text-xl flex items-center">{project.title}</h2>
-                    <span className="flex items-center">De: {project.author}</span>
-                  </a>
-                </article>
+                <ProjectCard project={project} key={index} />
               )
             })}
           </section>
         </section>
+
+        {
+          isCreateProjectShown
+            && <CreateProject ref={createProjectRef} onClose={handleCloseCreateProject} />
+        }
+
+        {
+          isSearchProjectsShown
+            && <SearchProjects onClose={handleCloseSearchProjects} projects={results} />
+        }
       </main>
     </>
   )
