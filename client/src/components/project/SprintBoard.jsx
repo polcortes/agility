37a132fa324a2eaf49ea62aae4bb8ceba2eb85a4
polcortes <pcortesgarcia.cf@iesesteveterradas.cat>
2024-04-03 16:30:29 +0200

    project page changes

---
+++ client/src/components/project/SprintBoard.jsx
@@ -0,0 +1,53 @@
+/* eslint-disable react/prop-types */
+// import { useState } from 'react';
+import TaskCard from '../project/TaskCard'
+
+const SprintBoard = ({ tasks }) => {
+
+  const createTask = () => {
+    
+  }
+
+  return (
+    <>
+      <div className='flex flex-col align-center justify-center p-5 bg-light-secondary-bg max-w-[330px] rounded-lg'>
+        <h3 className='font-subtitle font-bold text-2xl mb-5'>To-do</h3>
+        <ul className='flex flex-col rounded-lg overflow-hidden h-full max-h-full pr-5 gap-5 flex-1 overflow-y-scroll'>
+          { tasks.map(task => <li key={task.key}><TaskCard text={task.text} /></li>) }
+        </ul>
+        <span className='flex justify-center border-t-2 border-black pt-4'>
+          <button
+            className='bg-the-accent-color flex items-center justify-center rounded-md px-3 py-2 text-white font-medium hover:scale-105 transition-all gap-2'
+            onClick={() => createTask()}
+          >
+            <svg width="20" height="20" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.49991 0.876892C3.84222 0.876892 0.877075 3.84204 0.877075 7.49972C0.877075 11.1574 3.84222 14.1226 7.49991 14.1226C11.1576 14.1226 14.1227 11.1574 14.1227 7.49972C14.1227 3.84204 11.1576 0.876892 7.49991 0.876892ZM1.82707 7.49972C1.82707 4.36671 4.36689 1.82689 7.49991 1.82689C10.6329 1.82689 13.1727 4.36671 13.1727 7.49972C13.1727 10.6327 10.6329 13.1726 7.49991 13.1726C4.36689 13.1726 1.82707 10.6327 1.82707 7.49972ZM7.50003 4C7.77617 4 8.00003 4.22386 8.00003 4.5V7H10.5C10.7762 7 11 7.22386 11 7.5C11 7.77614 10.7762 8 10.5 8H8.00003V10.5C8.00003 10.7761 7.77617 11 7.50003 11C7.22389 11 7.00003 10.7761 7.00003 10.5V8H4.50003C4.22389 8 4.00003 7.77614 4.00003 7.5C4.00003 7.22386 4.22389 7 4.50003 7H7.00003V4.5C7.00003 4.22386 7.22389 4 7.50003 4Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
+            Afegir tasca
+          </button>
+        </span>
+      </div>
+
+      <div className='flex flex-col align-center justify-center p-5 bg-light-secondary-bg max-w-[330px] rounded-lg'>
+        <h3 className='font-subtitle font-bold text-2xl mb-5'>Doing</h3>
+        <ul className='flex flex-col rounded-lg overflow-hidden h-full max-h-full pr-5 gap-5 flex-1 overflow-y-scroll'>
+          { tasks.map(task => <li key={task.key}><TaskCard text={task.text} /></li>) }
+        </ul>
+      </div>
+
+      <div className='flex flex-col align-center justify-center p-5 bg-light-secondary-bg max-w-[330px] rounded-lg'>
+        <h3 className='font-subtitle font-bold text-2xl mb-5'>Testing</h3>
+        <ul className='flex flex-col rounded-lg overflow-hidden h-full max-h-full pr-5 gap-5 flex-1 overflow-y-scroll'>
+          { tasks.map(task => <li key={task.key}><TaskCard text={task.text} /></li>) }
+        </ul>
+      </div>
+
+      <div className='flex flex-col align-center justify-center p-5 bg-light-secondary-bg max-w-[330px] rounded-lg'>
+        <h3 className='font-subtitle font-bold text-2xl mb-5'>Done</h3>
+        <ul className='flex flex-col rounded-lg overflow-hidden h-full max-h-full pr-5 gap-5 flex-1 overflow-y-scroll'>
+          { tasks.map(task => <li key={task.key}><TaskCard text={task.text} /></li>) }
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default SprintBoard;
--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -11,12 +11,14 @@ import PageNotFound from '../pages/404'
 
 import ArrowIcon from '../assets/icons/arrow'
 
-import TaskCard from '../components/project/TaskCard'
+import SprintBoard from '../components/project/SprintBoard'
 
 const Project = () => {
   const { projectID } = useParams();
 
-  // const { project, setProject } = useState({})
+  const [ section, setSection ] = useState("SprintBoard") // TODO: setSection para setear si estamos en SprintBoard o estamos en Table o Chat.
+
+  const [ currProject, setCurrProject ] = useState(null)
 
   const [ projectState, setProjectState ] = useState("200")
 
@@ -35,12 +37,20 @@ const Project = () => {
     else {
       console.log(projectID)
       axios
-        .post('http://localhost:3000/getProjects', { // Conseguiré el project con la id que tengo
+        .post('http://localhost:3000/getProject', { // Conseguiré el project con la id que tengo
           token: 'WjGoEb_4VsUkC9vT3zPh6NmKJMl77ayn',
           projectID: projectID,
         })
-        .then(data => {console.table(data); setProjectState("200");}) // Comprobar si el usuario tiene permiso para acceder al proyecto.
-        .catch(() => {setProjectState("404"); console.log("No s'ha pogut carregar el projecte.")}) // tiene que ser 404
+        .then(res => { 
+          res = res.data
+          console.log(res.result);
+          setCurrProject(res.result);
+          setProjectState("200");
+        }) // Comprobar si el usuario tiene permiso para acceder al proyecto.
+        .catch(() => {
+          setProjectState("404"); 
+          console.log("No s'ha pogut carregar el projecte.")
+        }) // tiene que ser 404
     }
   }
 
@@ -71,7 +81,6 @@ const Project = () => {
   const [ latestSprint, setLatestSprint ] = useState({})
 
   const getLatestSprint = () => { // TODO(Pol): test it.
-    console.warn("He pasado por getLatestSprint")
     // const dates = [new Date("2024-03-20T18:31:12.158+00:00")]
     const dates = []
     sprints.forEach(sprint => dates.push(new Date(sprint.date)))
@@ -86,7 +95,6 @@ const Project = () => {
   const [ tasks, setTasks ] = useState([])
 
   const getTasks = () => {
-    console.warn("He pasado por getTasks")
     axios
       .post('http://localhost:3000/getTasksInSprint', {
         token: 'WjGoEb_4VsUkC9vT3zPh6NmKJMl77ayn',
@@ -125,10 +133,6 @@ const Project = () => {
     }
   }, [])
 
-  const createTask = () => {
-    
-  }
-
   return (
     <>
       <Helmet>
@@ -144,7 +148,7 @@ const Project = () => {
 
             <section className='h-fit pb-[21px] border-b-2 flex overflow-hidden mb-3'>
               <span className="flex size-16 mr-5 bg-black row-span-2 rounded-md box-border">a</span>
-              <span className='my-auto'>Nom del projecte</span>
+              <span className='my-auto'>{ currProject ? currProject.title : "Título mal." }</span>
             </section>
             
             <nav className='flex-1 flex flex-col'>
@@ -194,60 +198,10 @@ const Project = () => {
               </span>
             </header>
             <section id="main-project-container" className='bg-light-secondary-bg rounded-lg overflow-hidden grid max-h-full grid-cols-4 content-between p-5'>
-              <div className='flex flex-col align-center justify-center p-5 bg-light-secondary-bg max-w-[330px] rounded-lg'>
-                <h3 className='font-subtitle font-bold text-2xl mb-5'>To-do</h3>
-                <ul className='flex flex-col rounded-lg overflow-hidden h-full max-h-full pr-5 gap-5 flex-1 overflow-y-scroll'>
-                  { tasks.map(task => <li key={task.key}><TaskCard text={task.text} /></li>) }
-                </ul>
-                <span className='flex justify-center border-t-2 border-black pt-4'>
-                  <button
-                    className='bg-the-accent-color flex items-center justify-center rounded-md px-3 py-2 text-white font-medium hover:scale-105 transition-all gap-2'
-                    onClick={() => createTask(0)}
-                  >
-                    <svg width="20" height="20" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.49991 0.876892C3.84222 0.876892 0.877075 3.84204 0.877075 7.49972C0.877075 11.1574 3.84222 14.1226 7.49991 14.1226C11.1576 14.1226 14.1227 11.1574 14.1227 7.49972C14.1227 3.84204 11.1576 0.876892 7.49991 0.876892ZM1.82707 7.49972C1.82707 4.36671 4.36689 1.82689 7.49991 1.82689C10.6329 1.82689 13.1727 4.36671 13.1727 7.49972C13.1727 10.6327 10.6329 13.1726 7.49991 13.1726C4.36689 13.1726 1.82707 10.6327 1.82707 7.49972ZM7.50003 4C7.77617 4 8.00003 4.22386 8.00003 4.5V7H10.5C10.7762 7 11 7.22386 11 7.5C11 7.77614 10.7762 8 10.5 8H8.00003V10.5C8.00003 10.7761 7.77617 11 7.50003 11C7.22389 11 7.00003 10.7761 7.00003 10.5V8H4.50003C4.22389 8 4.00003 7.77614 4.00003 7.5C4.00003 7.22386 4.22389 7 4.50003 7H7.00003V4.5C7.00003 4.22386 7.22389 4 7.50003 4Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
-                    Afegir tasca
-                  </button>
-                </span>
-              </div>
-
-              <div className='flex flex-col align-center justify-center p-5 bg-light-secondary-bg max-w-[330px] rounded-lg'>
-                <h3 className='font-subtitle font-bold text-2xl mb-5'>Doing</h3>
-                <ul className='flex flex-col rounded-lg overflow-hidden h-full max-h-full pr-5 gap-5 flex-1 overflow-y-scroll'>
-                  { tasks.map(task => <li key={task.key}><TaskCard text={task.text} /></li>) }
-                </ul>
-                <span className='flex justify-center border-t-2 border-black pt-4'>
-                  <button
-                    className='bg-the-accent-color flex items-center justify-center rounded-md px-3 py-2 text-white font-medium hover:scale-105 transition-all gap-2'
-                    onClick={() => createTask(1)}
-                  >
-                    <svg width="20" height="20" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.49991 0.876892C3.84222 0.876892 0.877075 3.84204 0.877075 7.49972C0.877075 11.1574 3.84222 14.1226 7.49991 14.1226C11.1576 14.1226 14.1227 11.1574 14.1227 7.49972C14.1227 3.84204 11.1576 0.876892 7.49991 0.876892ZM1.82707 7.49972C1.82707 4.36671 4.36689 1.82689 7.49991 1.82689C10.6329 1.82689 13.1727 4.36671 13.1727 7.49972C13.1727 10.6327 10.6329 13.1726 7.49991 13.1726C4.36689 13.1726 1.82707 10.6327 1.82707 7.49972ZM7.50003 4C7.77617 4 8.00003 4.22386 8.00003 4.5V7H10.5C10.7762 7 11 7.22386 11 7.5C11 7.77614 10.7762 8 10.5 8H8.00003V10.5C8.00003 10.7761 7.77617 11 7.50003 11C7.22389 11 7.00003 10.7761 7.00003 10.5V8H4.50003C4.22389 8 4.00003 7.77614 4.00003 7.5C4.00003 7.22386 4.22389 7 4.50003 7H7.00003V4.5C7.00003 4.22386 7.22389 4 7.50003 4Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
-                    Afegir tasca
-                  </button>
-                </span>
-              </div>
-
-              <div className='flex flex-col align-center justify-center p-5 bg-light-secondary-bg max-w-[330px] rounded-lg'>
-                <h3 className='font-subtitle font-bold text-2xl mb-5'>Testing</h3>
-                <ul className='flex flex-col rounded-lg overflow-hidden h-full max-h-full pr-5 gap-5 flex-1 overflow-y-scroll'>
-                  { tasks.map(task => <li key={task.key}><TaskCard text={task.text} /></li>) }
-                </ul>
-                <span className='flex justify-center border-t-2 border-black pt-4'>
-                  <button
-                    className='bg-the-accent-color flex items-center justify-center rounded-md px-3 py-2 text-white font-medium hover:scale-105 transition-all gap-2'
-                    onClick={() => createTask(2)}
-                  >
-                    <svg width="20" height="20" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.49991 0.876892C3.84222 0.876892 0.877075 3.84204 0.877075 7.49972C0.877075 11.1574 3.84222 14.1226 7.49991 14.1226C11.1576 14.1226 14.1227 11.1574 14.1227 7.49972C14.1227 3.84204 11.1576 0.876892 7.49991 0.876892ZM1.82707 7.49972C1.82707 4.36671 4.36689 1.82689 7.49991 1.82689C10.6329 1.82689 13.1727 4.36671 13.1727 7.49972C13.1727 10.6327 10.6329 13.1726 7.49991 13.1726C4.36689 13.1726 1.82707 10.6327 1.82707 7.49972ZM7.50003 4C7.77617 4 8.00003 4.22386 8.00003 4.5V7H10.5C10.7762 7 11 7.22386 11 7.5C11 7.77614 10.7762 8 10.5 8H8.00003V10.5C8.00003 10.7761 7.77617 11 7.50003 11C7.22389 11 7.00003 10.7761 7.00003 10.5V8H4.50003C4.22389 8 4.00003 7.77614 4.00003 7.5C4.00003 7.22386 4.22389 7 4.50003 7H7.00003V4.5C7.00003 4.22386 7.22389 4 7.50003 4Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
-                    Afegir tasca
-                  </button>
-                </span>
-              </div>
-
-              <div className='flex flex-col align-center justify-center p-5 bg-light-secondary-bg max-w-[330px] rounded-lg'>
-                <h3 className='font-subtitle font-bold text-2xl mb-5'>Done</h3>
-                <ul className='flex flex-col rounded-lg overflow-hidden h-full max-h-full pr-5 gap-5 flex-1 overflow-y-scroll'>
-                  { tasks.map(task => <li key={task.key}><TaskCard text={task.text} /></li>) }
-                </ul>
-              </div>
+              {
+                section === "SprintBoard" 
+                  && <SprintBoard tasks={ tasks } />
+              }
             </section>
           </main>
         </section>
